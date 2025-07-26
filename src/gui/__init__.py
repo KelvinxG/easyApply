a74305915594f3726230bc5c293f53dd410b17ee
kelvin <tearteamoguy@gmail.com>
2025-07-26 22:40:39 +0700

    fix and add more packages for init files

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -5,7 +5,21 @@ Contains all PySide6-based user interface components for the EasyApply applicati
 """
 
 from .main_window import MainWindow
-from .widgets import *
-from .styles import *
+from .widgets import (
+    FileUploadWidget, KeywordTableWidget, ResultsWidget,
+    InfographicWidget, SettingsWidget, StatusWidget
+)
+from .styles import get_application_style, get_dark_theme_style, get_compact_style
 
-__all__ = ['MainWindow'] 
+__all__ = [
+    'MainWindow',
+    'FileUploadWidget', 
+    'KeywordTableWidget', 
+    'ResultsWidget',
+    'InfographicWidget', 
+    'SettingsWidget', 
+    'StatusWidget',
+    'get_application_style',
+    'get_dark_theme_style',
+    'get_compact_style'
+]
